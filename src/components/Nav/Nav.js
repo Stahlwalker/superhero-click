895fe4555f8a94b6a6b393962bdb6ec7559d7373
Luke Stahl <Stahl1109@gmail.com>
2018-04-17 13:17:04 -0500

    update navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,17 +4,17 @@ import "./Nav.css";
 const Nav = () => (
     <div className = "container-fluid">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Clicky Game</a>
+                <a className="navbar-brand">Clicky Game</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a className="nav-link">Click an image to begin!</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
+                        <a className="nav-link">Score: 0 | Top Score: 0</a>
                     </li>
                     </ul>
                 </div>
