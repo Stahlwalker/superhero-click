a48dcac718e93dd3ffd2ec8fe07846c0462f16ad
Luke Stahl <Stahl1109@gmail.com>
2018-04-19 09:25:15 -0500

    update jumbo

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -10,7 +10,7 @@ const Jumbo = () => (
     <div className="container">
       <div className="jumbotron">
         <h1>Clicky Game</h1>
-        <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+        <h2>Click on an image to earn points, but don't click it more than once!</h2>
       </div>
     </div>
   </div>
