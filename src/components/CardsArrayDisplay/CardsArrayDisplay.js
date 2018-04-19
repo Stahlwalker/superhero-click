b2e8f91f02cb533af89de0ad04d3c98317d33cce
Luke Stahl <Stahl1109@gmail.com>
2018-04-19 09:41:42 -0500

    add homepage for deploy

--- src/components/CardsArrayDisplay/CardsArrayDisplay.js
+++ src/components/CardsArrayDisplay/CardsArrayDisplay.js
@@ -16,6 +16,27 @@ handleClick = () =>
     const shuffleImages = this.state.cardsArr.sort((a,b) => 0.5 - Math.random())
     this.setState({cardsArr: shuffleImages}) 
 
+    // let score = this.state.clickedHeroIDs;
+    // if(clickedHeroIDs.includes(id)){
+    //     this.setState({ clickedHeroIDs: [], score: 0, status:  "Game Over! You lost. Click to play again!" });
+    //     return;
+    //   }else{
+    //     clickedHeroIDs.push(id)
+  
+    //     if(clickedHeroIDs.length === 9){
+    //       this.setState({score: 9, status: "You Won! Great Job, Smartie! Click to play again!", clickedPuppyIds: []});
+    //       console.log('You Win');
+    //       return;
+    //     }
+  
+    //     this.setState({ cardsArr, clickedHeroIDs, score: clickedHeroIDs.length, status: " " });
+  
+    //     for (let i = cardsArr.length - 1; i > 0; i--) {
+    //       let j = Math.floor(Math.random() * (i + 1));
+    //       [cardsArr[i], cardsArr[j]] = [cardsArr[j], cardsArr[i]];
+    //     }
+    // }
+
 }
 
     render(){
