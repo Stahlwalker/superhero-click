5a387c4145a40d95ec68b33e813ada3b7da36ca9
Luke Stahl <Stahl1109@gmail.com>
2018-04-19 09:07:12 -0500

    shuffleImages not working

--- src/components/CardsArrayDisplay/CardsArrayDisplay.js
+++ src/components/CardsArrayDisplay/CardsArrayDisplay.js
@@ -7,47 +7,28 @@ import './CardsArrayDisplay.css';
 
 class CardsArrayDisplay extends Component{
 state={
-       cards
+    cardsArr: cards
    }
 
-//handleClick(){}
 handleClick()
 {
-    console.log("hello there");
-    
+    console.log("click working");
+    const shuffleImages = this.state.cardsArr.sort((a,b) => 0.5 - Math.random())
+    // const shuffleImages = this.state.cardsArr.sort(function(a,b) { 0.5 - Math.random()})
+    this.setState({cardsArr: shuffleImages}) 
+
 }
 
     render(){
-        console.log(this.state.cards)
+        console.log(this.state.cardsArr)
         return(
-            <div>{this.state.cards.map(card=>(
+            <div>{this.state.cardsArr.map(card=>(
               <img onClick={this.handleClick} src={require(`../Cards/Images/${card.img}`)} alt="pic"/>
                 ))} 
                 </div>
-            
         )
     }
 
 }
-
-
-
-
-// CardsArray.forEach(item => {
-//     // Create a div
-//     const card = document.createElement('div');
-  
-//     // Apply a card class to that div
-//     card.classList.add('card');
-  
-//     // Set the data-name attribute of the div to the cardsArray name
-//     card.dataset.name = item.name;
-  
-//     // Apply the background image of the div to the cardsArray image
-//     card.style.backgroundImage = `url(${item.img})`;
-  
-//     // Append the div to the grid section
-//     grid.appendChild(card);
-//   });
 
   export default CardsArrayDisplay;
